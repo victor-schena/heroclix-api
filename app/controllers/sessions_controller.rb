832c0155b6efbbd23ea6a4903e6e1802b6a132c8
victor-schena <victor.schena@ateliware.com>
2020-12-06 14:42:01 -0300

    Basic authentication finished.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,16 @@
 class SessionsController < ApplicationController
   before_action :block_access, except: [:destroy]
   def create
-            @user = User.find_by(email: params[:session][:email].downcase)
-            if @user && @user.authenticate(params[:session][:password])
-                        sign_in(@user)
-            redirect_to @user
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+                sign_in(@user)
+    redirect_to home_index_url
     else
-            render 'new'
+      render 'new'
     end
   end
   def destroy
     sign_out
-redirect_to root_url
-end
+    redirect_to root_url
+  end
 end
